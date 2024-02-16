a2d190bd3118c5ca29de413ffcb53cc7c852ff4d
Ibrahim11511 <109541964+Ibrahim11511@users.noreply.github.com>
2024-02-16 22:39:55 +0200

    Delete Non-usage function

--- script.js
+++ script.js
@@ -2,11 +2,7 @@ const inInput = document.querySelector('.in-input')
 const submitBtn = document.querySelector('.calculator form .submit')
 const resultWrapper = document.querySelector('.result-container .result')
 
-function incheToMetarConvert(number) {
-    return parseInt(number * 0.0254)
-}
-
 submitBtn.addEventListener('click', () => {
     let result = parseInt(inInput.value) * 0.0254
-    resultWrapper.textContent = `${inInput.value} inch = ${result} Metars`
+    resultWrapper.textContent = `${inInput.value} inch = ${result} Meters`
 })
